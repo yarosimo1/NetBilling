342ff0ac9fbb6b6785c179e627981978120317c3
Yaroslav Tamliani <yaroslav.tamliane@yandex.ru>
2026-08-05 16:40:59 +0700

    подготовлена базовая архитектура проекта

---
+++ src/main/java/dbg/netbill/NetBillApplication.java
@@ -0,0 +1,10 @@
+package dbg.netbill;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetBillApplication {
+    public static void main(String[] args) {
+
+    }
+}
